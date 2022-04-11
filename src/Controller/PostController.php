0f677c1189a46eb68be7ba875580283e0b59be3a
Rafaelkoebel <rafael.koebel@gmail.com>
2022-04-11 16:56:16 +0200

    découpage et liens

---
+++ src/Controller/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('post/home.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    // #[Route('/post', name: 'post_view')]
+    // public function post(): Response
+    // {
+    //     return $this->render('post/view.html.twig', [
+    //         'controller_name' => 'PostController',
+    //     ]);
+    // }
+
+    #[Route('/post/{id}', name: 'post_view')]
+    public function postid($id): Response
+    {
+        return $this->render('post/view.html.twig', [
+            'controller_name' => 'PostController',
+            'id' => $id,
+        ]);
+    }
+}
