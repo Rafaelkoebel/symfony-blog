fbd6ee68ba2decd87d6bd8f209613b66d68fd82b
Rafaelkoebel <rafael.koebel@gmail.com>
2022-04-19 17:03:02 +0200

    fin form post; dev home; dev view post; slug

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PostController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+        //dd($posts);
+
         return $this->render('post/home.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
         ]);
     }
 
@@ -26,26 +30,35 @@ class PostController extends AbstractController
     //         'controller_name' => 'PostController',
     //     ]);
     // }
-
-    #[Route('/post/{id}', name: 'post_view')]
-    public function postid($id): Response
-    {
-        return $this->render('post/view.html.twig', [
-            'post' => [
-                'title' => 'Le titre de l\'article',
-                'content' => 'Mon super contenu',
-            ]
-        ]);
-    }
-
+    
     #[Route('/post/add', name: 'post_add')]
     public function addPost(Request $request): Response
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        $post->setUser($this->getUser());
+        $post->setActive(false);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+        return $this->redirectToRoute('home');
+    }
+    
+
         return $this->render('post/add.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+    
+    #[Route('/post/{slug}', name: 'post_view')]
+    public function postid(Post $post): Response
+    {
+        return $this->render('post/view.html.twig', [
+            'post' => $post
+        ]);
+    }
+
 }
