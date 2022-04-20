6440c2112d84ea7a02ec9fc8d11d6a311b8a3602
Rafaelkoebel <rafael.koebel@gmail.com>
2022-04-20 12:32:28 +0200

    controller category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category/{slug}', name: 'app_category')]
+    public function index(Category $category): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
