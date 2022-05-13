21bd6bc860117e52ba4198fd36cb7f02c6df12d0
Rafaelkoebel <rafael.koebel@gmail.com>
2022-05-13 12:10:09 +0200

    mise en prod heroku

--- exports/symfony-blog.sql
+++ exports/symfony-blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : mar. 19 avr. 2022 à 17:04
--- Version du serveur : 10.4.22-MariaDB
--- Version de PHP : 8.1.2
+-- Hôte : localhost:3306
+-- Généré le : ven. 13 mai 2022 à 09:10
+-- Version du serveur :  5.7.24
+-- Version de PHP : 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +39,10 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `parent_id`, `name`, `slug`) VALUES
-(1, NULL, 'Surf', 'surf');
+(1, NULL, 'Surf', 'surf'),
+(2, NULL, 'ski de randonnée', 'ski-de-randonnee'),
+(3, 4, 'skate', 'skate'),
+(4, NULL, 'sports de glisse', 'sports-de-glisse');
 
 -- --------------------------------------------------------
 
@@ -58,11 +61,11 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20220412114742', '2022-04-12 11:55:51', 763),
-('DoctrineMigrations\\Version20220412131004', '2022-04-12 13:12:54', 114),
-('DoctrineMigrations\\Version20220413095056', '2022-04-14 07:08:18', 1319),
-('DoctrineMigrations\\Version20220413101745', '2022-04-14 07:08:20', 2199),
-('DoctrineMigrations\\Version20220414091137', '2022-04-14 09:11:57', 725);
+('DoctrineMigrations\\Version20220412114742', '2022-05-04 09:48:39', 128),
+('DoctrineMigrations\\Version20220412131004', '2022-05-04 09:48:39', 52),
+('DoctrineMigrations\\Version20220413095056', '2022-05-04 09:48:40', 130),
+('DoctrineMigrations\\Version20220413101745', '2022-05-04 09:48:40', 158),
+('DoctrineMigrations\\Version20220414091137', '2022-05-04 09:48:40', 91);
 
 -- --------------------------------------------------------
 
@@ -79,6 +82,13 @@ CREATE TABLE `messenger_messages` (
   `available_at` datetime NOT NULL,
   `delivered_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `messenger_messages`
+--
+
+INSERT INTO `messenger_messages` (`id`, `body`, `headers`, `queue_name`, `created_at`, `available_at`, `delivered_at`) VALUES
+(1, 'O:36:\\\"Symfony\\\\Component\\\\Messenger\\\\Envelope\\\":2:{s:44:\\\"\\0Symfony\\\\Component\\\\Messenger\\\\Envelope\\0stamps\\\";a:1:{s:46:\\\"Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\\";a:1:{i:0;O:46:\\\"Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\\":1:{s:55:\\\"\\0Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\0busName\\\";s:21:\\\"messenger.bus.default\\\";}}}s:45:\\\"\\0Symfony\\\\Component\\\\Messenger\\\\Envelope\\0message\\\";O:51:\\\"Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\\":2:{s:60:\\\"\\0Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\0message\\\";O:39:\\\"Symfony\\\\Bridge\\\\Twig\\\\Mime\\\\TemplatedEmail\\\":4:{i:0;s:41:\\\"registration/confirmation_email.html.twig\\\";i:1;N;i:2;a:3:{s:9:\\\"signedUrl\\\";s:165:\\\"http://127.0.0.1:8000/verify/email?expires=1652168143&signature=rBQG0h06efgMjc41XNtdoDUvjm7rWWzOtSNuXZ%2FBNkA%3D&token=IG9Z2rr1ppAUdjyWFRQfegbHQ5uBRalLZy3EjX807B8%3D\\\";s:19:\\\"expiresAtMessageKey\\\";s:26:\\\"%count% hour|%count% hours\\\";s:20:\\\"expiresAtMessageData\\\";a:1:{s:7:\\\"%count%\\\";i:1;}}i:3;a:6:{i:0;N;i:1;N;i:2;N;i:3;N;i:4;a:0:{}i:5;a:2:{i:0;O:37:\\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\\":2:{s:46:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\0headers\\\";a:3:{s:4:\\\"from\\\";a:1:{i:0;O:47:\\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\\":5:{s:58:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\0addresses\\\";a:1:{i:0;O:30:\\\"Symfony\\\\Component\\\\Mime\\\\Address\\\":2:{s:39:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0address\\\";s:15:\\\"noreply@xyz.com\\\";s:36:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0name\\\";s:3:\\\"moi\\\";}}s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\\";s:4:\\\"From\\\";s:56:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\\";i:76;s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\\";N;s:53:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\\";s:5:\\\"utf-8\\\";}}s:2:\\\"to\\\";a:1:{i:0;O:47:\\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\\":5:{s:58:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\0addresses\\\";a:1:{i:0;O:30:\\\"Symfony\\\\Component\\\\Mime\\\\Address\\\":2:{s:39:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0address\\\";s:19:\\\"salimatou@gmail.com\\\";s:36:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0name\\\";s:0:\\\"\\\";}}s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\\";s:2:\\\"To\\\";s:56:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\\";i:76;s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\\";N;s:53:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\\";s:5:\\\"utf-8\\\";}}s:7:\\\"subject\\\";a:1:{i:0;O:48:\\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\UnstructuredHeader\\\":5:{s:55:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\UnstructuredHeader\\0value\\\";s:25:\\\"Please Confirm your Email\\\";s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\\";s:7:\\\"Subject\\\";s:56:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\\";i:76;s:50:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\\";N;s:53:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\\";s:5:\\\"utf-8\\\";}}}s:49:\\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\0lineLength\\\";i:76;}i:1;N;}}}s:61:\\\"\\0Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\0envelope\\\";N;}}', '[]', 'default', '2022-05-10 08:35:44', '2022-05-10 08:35:44', NULL);
 
 -- --------------------------------------------------------
 
@@ -103,10 +113,8 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `user_id`, `category_id`, `title`, `slug`, `content`, `image`, `created_at`, `active`) VALUES
-(1, 5, 1, 'titre 2', 'titre-2', 'contenu 2', 'https://www.leparisien.fr/resizer/LjfQdowFD9P41GaqTLvKLUDdot4=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/IRGRVPHIVZQM3M4EPI6AK6MTBA.jpg', '2022-04-19 08:38:30', 0),
-(2, 5, 1, 'titre 2', 'titre-2-1', 'contenu 2', 'https://www.leparisien.fr/resizer/LjfQdowFD9P41GaqTLvKLUDdot4=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/IRGRVPHIVZQM3M4EPI6AK6MTBA.jpg', '2022-04-19 08:38:51', 0),
-(3, 5, 1, 'titre 3', 'titre-3', 'contenu 3', 'https://www.leparisien.fr/resizer/LjfQdowFD9P41GaqTLvKLUDdot4=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/IRGRVPHIVZQM3M4EPI6AK6MTBA.jpg', '2022-04-19 08:40:42', 0),
-(4, 5, 1, 'titre 3', 'titre-3-1', '<p><img alt=\"Une personne faisant du surf face au coucher du soleil\" src=\"https://cdn.radiofrance.fr/s3/cruiser-production/2021/08/696b8bea-3893-4a18-b549-515a4c641d93/838_gettyimages-982747408.jpg\" /></p>\r\n\r\n<p>Peut-on dater l&#39;invention du surf ? Que repr&eacute;sente cette pratique dans la culture hawa&iuml;enne ? Comment est n&eacute;e la surf culture ? Dans quelle mesure peut-on consid&eacute;rer qu&#39;elle a touch&eacute; tous les domaines artistiques ? Comment ont &eacute;volu&eacute; les planches de surf ?</p>\r\n\r\n\r\n\r\n<p>Une personne faisant du surf face au coucher du soleil&bull;&nbsp;Cr&eacute;dits :&nbsp;<em>Marco Petracci</em>&nbsp;-&nbsp;<em>Getty</em></p>\r\n\r\n<p><em>&ldquo;Glisser sur la vague et se fondre avec elle&rdquo;</em>&nbsp;tel est le sens du mot&nbsp;<em>&ldquo;he&rsquo;enalu&rdquo;</em>, l&rsquo;anc&ecirc;tre hawa&iuml;en de ce qui est devenu un sport de masse&nbsp;: le surf.</p>\r\n\r\n<p>De l&rsquo;archipel perdu dans le Pacifique, aux c&ocirc;tes californiennes, puis aux rivages houleux du monde entier, le surf est devenu en deux si&egrave;cles un symbole&nbsp;: celui d&rsquo;un sport en harmonie avec les &eacute;l&eacute;ments et celui d&rsquo;une culture globale.</p>\r\n\r\n<p>C&rsquo;est donc dans l&rsquo;histoire d&rsquo;une vague sportive et culturelle que nous allons plonger.</p>', 'https://cdn.radiofrance.fr/s3/cruiser-production/2021/08/696b8bea-3893-4a18-b549-515a4c641d93/838_gettyimages-982747408.webp', '2022-04-19 09:48:10', 0);
+(1, 2, 1, 'test1', 'test1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg', '2022-05-10 08:36:04', 1),
+(2, 2, 1, 'test2', 'test2', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 'https://media.istockphoto.com/photos/picturesque-morning-in-plitvice-national-park-colorful-spring-scene-picture-id1093110112?k=20&m=1093110112&s=612x612&w=0&h=3OhKOpvzOSJgwThQmGhshfOnZTvMExZX2R91jNNStBY=', '2022-05-10 08:37:45', 0);
 
 -- --------------------------------------------------------
 
@@ -122,18 +130,6 @@ CREATE TABLE `reset_password_request` (
   `requested_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `expires_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `reset_password_request`
---
-
-INSERT INTO `reset_password_request` (`id`, `user_id`, `selector`, `hashed_token`, `requested_at`, `expires_at`) VALUES
-(1, 5, 'rBX7GRGareSBeFGLeA9C', 'UkLD7jpuWL5Z2kasUknx3vfwACazzwPQlBOxKD9D+DM=', '2022-04-14 09:13:56', '2022-04-14 10:13:56'),
-(2, 4, 'cMxKj5zEP7mDHH0H8pbN', 'qQmAH7LB/cgEsiKWNDaj8Uh4Eu5Bdt3MQ/6F7UVcFls=', '2022-04-14 09:17:56', '2022-04-14 10:17:56'),
-(3, 6, 'ay9UhI8N8eyseBbiSy2P', 'fFS32E8ae5G17e8IA7I4VAxudOxc9Ryl2ZzGBdC/6NM=', '2022-04-14 09:48:25', '2022-04-14 10:48:25'),
-(4, 5, 'zvjLLLdtpl3hFK6BWlfz', 'AxLUsmFU5AiNKaMectN6yRHUAlUOWkPyaIUbkvp4V4g=', '2022-04-14 11:21:08', '2022-04-14 12:21:08'),
-(5, 6, 'sVeAmoNs27RL1or2Uzjz', 'dh9aiU7n9V9l4CrMGl1bBYuwugJ6MjlX7VNsyOGpQCw=', '2022-04-14 11:34:34', '2022-04-14 12:34:34'),
-(6, 4, 'UVxhgy2tVBqnrrU0iUek', 'gSVj0gQ4KPWleGzPJY7GMZDgN/1I53BCVr159td9Naw=', '2022-04-14 11:37:13', '2022-04-14 12:37:13');
 
 -- --------------------------------------------------------
 
@@ -154,12 +150,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`) VALUES
-(1, 'test1@test.com', '[]', '$2y$13$iwrEJsq9xQaFXSwC5IvQQuNfexXn0oCiU8HVTdCxAcNbjD6abOBEi', 0),
-(2, 'salut.bonsoir@free.fr', '[]', '$2y$13$h6VjVASFkf81JkMwxF0Q3umfrq1Iw2Nn2T/xVuoPu3zaTeSVuoGRu', 0),
-(3, 'philippe@gmail.com', '[]', '$2y$13$6J8Rs.wbsHDQtdabNQYhTuYBdWDx6.bgRaSOV.SZK8qwFX3FWXdom', 0),
-(4, 'rafael.koebell@gmail.com', '[]', '$2y$13$FLI2psS7QtNj4V7zEmYEN.i/39kaEyBi0GkbSWzX3kzWryrFPhhN.', 0),
-(5, 'salimatou@free.fr', '[]', '$2y$13$EuliDIA/klD4dLL.3nJJ7uUsfwDqQ.ptpmlI.3/vXJphbXZKCdUFa', 1),
-(6, 'salut.bonsoir@gmail.com', '[]', '$2y$13$8iBq909gZxRLX5/Yabiiau1QGrQvG72XDlR1YVd/ksSWxUMORdowG', 1);
+(1, 'rafael.koebel@gmail.com', '[\"ROLE_ADMIN\"]', '123456', 1),
+(2, 'salimatou@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$mFiKL.hoCvZpp5MfJuBS7uV5qI/WdjWKiXbiy9C92zFk4utdx2btO', 0);
 
 --
 -- Index pour les tables déchargées
@@ -217,31 +209,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `messenger_messages`
 --
 ALTER TABLE `messenger_messages`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `reset_password_request`
 --
 ALTER TABLE `reset_password_request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
